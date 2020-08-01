53d9dd6d4c70a703922bb34599b241578f8917d2
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-01 01:55:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_dogs << "Luna"
-  puts famous_cats.inspect
+  puts my_dogs.inspect
 end
 
 def add_element_to_start_of_array(array, element)
