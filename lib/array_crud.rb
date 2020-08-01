fafebcad625fa9445e16c4a70620a4ce430272c3
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-01 02:26:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Sophia", "Max", "Stripes", "Buddy"]
-  element = "array"
-  array.push(element)
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
