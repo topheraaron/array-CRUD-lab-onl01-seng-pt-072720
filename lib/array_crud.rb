a4be3c45428f3633a8fb51c3d942cfcf6ce1fa34
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-01 02:06:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,13 +3,13 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_dogs = ["Sophia", "Max", "Stripes", "Buddy"]
+  ["Sophia", "Max", "Stripes", "Buddy"]
 end
 
 def add_element_to_end_of_array(array, element)
-  my_dogs = ["Sophia", "Max", "Stripes"]
-  my_dogs << "Luna"
-  puts my_dogs.inspect
+  array = ["Sophia", "Max", "Stripes", "Buddy"]
+  element = "array"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
